b947a829509b72c4548549c6441a3eb1495db221
rT0NY <ajvryt.1514@gmail.com>
2026-06-09 01:02:13 -0600

    chore: v2.1.5

--- src/features/productos/ProductosPage.jsx
+++ src/features/productos/ProductosPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useRef } from 'react'
+import { useState, useMemo, useRef, useEffect } from 'react'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import {
   Plus, Search, Package, Filter, RefreshCw, Archive,
